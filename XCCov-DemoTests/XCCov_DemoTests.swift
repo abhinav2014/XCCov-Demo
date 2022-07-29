6cc3e5a7860f1a8cc0cd3eaec98ca80a69491dc7
abhinav-ti <abhinav.dobhal@telusinternational.com>
2022-07-29 18:55:34 +0530

    updated test files

--- XCCov-DemoTests/XCCov_DemoTests.swift
+++ XCCov-DemoTests/XCCov_DemoTests.swift
@@ -30,6 +30,7 @@ class XCCov_DemoTests: XCTestCase {
         // This is an example of a performance test case.
         self.measure {
             // Put the code you want to measure the time of here.
+            
         }
     }
     
