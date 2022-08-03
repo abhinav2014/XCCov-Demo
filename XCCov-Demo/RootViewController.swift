10e206b45df4c91f4a987ddccc33cf00c01c9445
abhinav-ti <abhinav.dobhal@telusinternational.com>
2022-08-04 01:03:53 +0530

    message

--- XCCov-Demo/RootViewController.swift
+++ XCCov-Demo/RootViewController.swift
@@ -38,7 +38,8 @@ class RootViewController: UIViewController, UIPageViewControllerDelegate {
 
         self.pageViewController!.didMove(toParentViewController: self)
     }
-
+    
+    /// did Receive Memory Warning
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
